e904a7cad0d9602acb8128b08f51236e1e6719b7
Sergio Barrera <sdbeng@gmail.com>
2018-01-24 22:04:40 -0800

    Add final nested routes

--- src/App.js
+++ src/App.js
@@ -4,63 +4,6 @@ import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 // import './App.css'
 
 //nested routes app
-
-const Home = () => {
-  return (
-    <h1>Home</h1>
-  )
-}
-const About = () => {
-  return (
-    <h1>About</h1>
-  )
-}
-const Topic = () => {
-  return (
-    <h1>Topic</h1>
-  )
-}
-const Topics = ({match}) => {
-
-  return (
-    <div>
-      <h1>Topics</h1>
-      <ul>
-        {topics.map(({name, id}) => console.log('log match', match) || (
-          <li key={id}>
-            <Link to={`/topics/${id}`}>{name}</Link>
-          </li>
-        ))}
-      </ul>
-      <hr/>
-      <Route path={`/topics/:topicId`} component={Topic} />
-    </div>
-  )
-}
-
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div style={{width: 1000, margin: '0 auto'}}>
-            <h1 className="App-title">Welcome to React</h1>
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/about'>About</Link></li>
-              <li><Link to='/topics'>Topics</Link></li>
-            </ul>
-            <hr/>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/topics' component={Topics} />
-        </div>
-      </Router>
-
-    );
-  }
-}
-
 const topics = [
   {
     name: 'React Router',
@@ -120,5 +63,95 @@ const topics = [
     ]
   }
 ]
+
+const Home = () => {
+  return (
+    <h1>Home</h1>
+  )
+}
+const About = () => {
+  return (
+    <h1>About</h1>
+  )
+}
+
+const Resource = ({match}) => {
+  const topic = topics.find(({ id }) => id === match.params.topicId)
+    .resources.find(({ id }) => id === match.params.subId)
+
+  return (
+    <div>
+      <h3>{topic.name}</h3>
+      <p>{topic.description}</p>
+      <a href={topic.url}>More info.</a>
+    </div>
+  )
+}
+
+function Topic ({ match }) {
+  const topic = topics.find(({ id }) => id === match.params.topicId)
+
+  return (
+    <div>
+      <h2>{topic.name}</h2>
+      <p>{topic.description}</p>
+
+      <ul>
+        {topic.resources.map((sub) => console.log('log match', match) || (
+          <li key={sub.id}>
+            <Link to={`${match.url}/${sub.id}`}>{sub.name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <Route path={`${match.path}/:subId`} component={Resource} />
+    </div>
+  )
+}
+const Topics = ({match}) => {
+
+  return (
+    <div>
+      <h1>Topics</h1>
+      <ul>
+        {topics.map(({name, id}) => (
+          <li key={id}>
+            {/* if someone decide to later change the name of topics to concepts, try this prev. console logging the match param */}
+            <Link to={`${match.url}/${id}`}>{name}</Link>
+            {/* <Link to={`/topics/${id}`}>{name}</Link> */}
+          </li>
+        ))}
+      </ul>
+      <hr/>
+      <Route path={`/topics/:topicId`} component={Topic} />
+    </div>
+  )
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div style={{width: 1000, margin: '0 auto'}}>
+            <h1 className="App-title">Welcome to React</h1>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/topics'>Topics</Link></li>
+            </ul>
+            <hr/>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/topics' component={Topics} />
+        </div>
+      </Router>
+
+    );
+  }
+}
+
 
 export default App
