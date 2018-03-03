222b9c070eb8c9fdb45295c728343e2e99ab758a
Sergio Barrera <sdbeng@gmail.com>
2018-03-02 17:22:54 -0800

    Add Storepicker component

---
+++ src/components/Storepicker.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+class Storepicker extends Component {
+    render(){
+        return (
+            <div>Hello Storepicker</div>
+        )
+    }
+}
+export default Storepicker
